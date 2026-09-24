6ee7e512a5febf97dd6b580d522330d26a465f6b
fetrakd <fetra@khaleej-group.com>
2026-09-24 17:32:11 +0300

    Fix portfolio projects and data

--- src/components/achievementCard/AchievementCard.js
+++ src/components/achievementCard/AchievementCard.js
@@ -1,42 +1,75 @@
 import React from "react";
 import "./AchievementCard.scss";
 
-export default function AchievementCard({cardInfo, isDark}) {
+export default function AchievementCard({ cardInfo, isDark }) {
   function openUrlInNewTab(url, name) {
     if (!url) {
       console.log(`URL for ${name} not found`);
       return;
     }
-    var win = window.open(url, "_blank");
-    win.focus();
+
+    const win = window.open(url, "_blank");
+
+    if (win) {
+      win.focus();
+    }
   }
 
+  // Supporte les deux formats utilisés par le template :
+  // footer et footerLink
+  const footerLinks = cardInfo.footer || cardInfo.footerLink || [];
+
   return (
-    <div className={isDark ? "dark-mode certificate-card" : "certificate-card"}>
+    <div
+      className={
+        isDark
+          ? "dark-mode certificate-card"
+          : "certificate-card"
+      }
+    >
       <div className="certificate-image-div">
         <img
           src={cardInfo.image}
           alt={cardInfo.imageAlt || "Card Thumbnail"}
           className="card-image"
-        ></img>
+        />
       </div>
+
       <div className="certificate-detail-div">
-        <h5 className={isDark ? "dark-mode card-title" : "card-title"}>
+        <h5
+          className={
+            isDark
+              ? "dark-mode card-title"
+              : "card-title"
+          }
+        >
           {cardInfo.title}
         </h5>
-        <p className={isDark ? "dark-mode card-subtitle" : "card-subtitle"}>
-          {cardInfo.description}
+
+        <p
+          className={
+            isDark
+              ? "dark-mode card-subtitle"
+              : "card-subtitle"
+          }
+        >
+          {cardInfo.description || cardInfo.subtitle}
         </p>
       </div>
+
       <div className="certificate-card-footer">
-        {cardInfo.footer.map((v, i) => {
+        {footerLinks.map((v, i) => {
           return (
             <span
               key={i}
               className={
-                isDark ? "dark-mode certificate-tag" : "certificate-tag"
+                isDark
+                  ? "dark-mode certificate-tag"
+                  : "certificate-tag"
               }
-              onClick={() => openUrlInNewTab(v.url, v.name)}
+              onClick={() =>
+                openUrlInNewTab(v.url, v.name)
+              }
             >
               {v.name}
             </span>
--- src/portfolio.js
+++ src/portfolio.js
@@ -1,67 +1,69 @@
 /* Change this file to get your personal Portfolio */
 
-// To change portfolio colors globally go to the  _globalColor.scss file
-
 import emoji from "react-easy-emoji";
-import splashAnimation from "./assets/lottie/splashAnimation"; // Rename to your file name for custom animation
+import splashAnimation from "./assets/lottie/splashAnimation";
 
 // Splash Screen
-
 const splashScreen = {
-  enabled: true, // set false to disable splash screen
+  enabled: true,
   animation: splashAnimation,
-  duration: 2000 // Set animation duration as per your animation
+  duration: 2000
+};
+
+// Illustration
+const illustration = {
+  animated: true
 };
 
 // Summary And Greeting Section
-
-const illustration = {
-  animated: true // Set to false to use static SVG
-};
-
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Fetra Razafindrakoto",
+  title: "Bonjour, je suis Fetra",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "Intégrateur Web Senior et développeur Frontend depuis 2006. Je travaille sur des projets e-commerce et web avec Shopify, Magento, WordPress, JavaScript, React, Node.js et PHP."
   ),
-  resumeLink:
-    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
-  displayGreeting: true // Set false to hide this section, defaults to true
+  resumeLink: "",
+  displayGreeting: true
 };
 
 // Social Media Links
-
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
-  // Instagram, Twitter and Kaggle are also supported in the links!
-  // To customize icons and social links, tweak src/components/SocialMedia
-  display: true // Set true to display this section, defaults to false
+  github: "https://github.com/Andrianimehy",
+  linkedin: "",
+  gmail: "",
+  gitlab: "",
+  facebook: "",
+  medium: "",
+  stackoverflow: "",
+  display: true
 };
 
 // Skills Section
-
 const skillsSection = {
-  title: "What I do",
-  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  title: "Ce que je fais",
+  subTitle:
+    "INTÉGRATION WEB, E-COMMERCE ET DÉVELOPPEMENT FRONTEND",
+
   skills: [
     emoji(
-      "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
+      "⚡ Intégration de maquettes graphiques et développement d'interfaces web modernes et responsives"
     ),
-    emoji("⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"),
     emoji(
-      "⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
+      "⚡ Création et personnalisation de boutiques e-commerce avec Shopify, Magento et WooCommerce"
+    ),
+    emoji(
+      "⚡ Développement frontend avec JavaScript, React et intégration d'API"
+    ),
+    emoji(
+      "⚡ Développement backend avec Node.js, Express et PHP"
+    ),
+    emoji(
+      "⚡ Optimisation des performances, SEO technique et responsive design"
+    ),
+    emoji(
+      "⚡ Migration, maintenance et amélioration de sites web existants"
     )
   ],
-
-  /* Make Sure to include correct Font Awesome Classname to view your icon
-https://fontawesome.com/icons?d=gallery */
 
   softwareSkills: [
     {
@@ -73,317 +75,306 @@ https://fontawesome.com/icons?d=gallery */
       fontAwesomeClassname: "fab fa-css3-alt"
     },
     {
-      skillName: "sass",
-      fontAwesomeClassname: "fab fa-sass"
-    },
-    {
       skillName: "JavaScript",
       fontAwesomeClassname: "fab fa-js"
     },
     {
-      skillName: "reactjs",
+      skillName: "React",
       fontAwesomeClassname: "fab fa-react"
     },
     {
-      skillName: "nodejs",
+      skillName: "Node.js",
       fontAwesomeClassname: "fab fa-node"
     },
     {
-      skillName: "swift",
-      fontAwesomeClassname: "fab fa-swift"
+      skillName: "PHP",
+      fontAwesomeClassname: "fab fa-php"
+    },
+    {
+      skillName: "WordPress",
+      fontAwesomeClassname: "fab fa-wordpress"
+    },
+    {
+      skillName: "Shopify",
+      fontAwesomeClassname: "fab fa-shopify"
+    },
+    {
+      skillName: "Git",
+      fontAwesomeClassname: "fab fa-git-alt"
     },
     {
       skillName: "npm",
       fontAwesomeClassname: "fab fa-npm"
     },
     {
-      skillName: "sql-database",
+      skillName: "Database",
       fontAwesomeClassname: "fas fa-database"
-    },
-    {
-      skillName: "aws",
-      fontAwesomeClassname: "fab fa-aws"
-    },
-    {
-      skillName: "firebase",
-      fontAwesomeClassname: "fas fa-fire"
-    },
-    {
-      skillName: "python",
-      fontAwesomeClassname: "fab fa-python"
-    },
-    {
-      skillName: "docker",
-      fontAwesomeClassname: "fab fa-docker"
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+
+  display: true
 };
 
 // Education Section
-
 const educationInfo = {
-  display: true, // Set false to hide this section, defaults to true
+  display: true,
+
   schools: [
     {
-      schoolName: "Harvard University",
+      schoolName: "Licence en Informatique de Gestion",
       logo: require("./assets/images/harvardLogo.png"),
-      subHeader: "Master of Science in Computer Science",
-      duration: "September 2017 - April 2019",
-      desc: "Participated in the research of XXX and published 3 papers.",
-      descBullets: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-      ]
+      subHeader: "Informatique de Gestion",
+      duration: "2019",
+      desc: "Formation supérieure en informatique de gestion."
     },
     {
-      schoolName: "Stanford University",
+      schoolName: "DTS en Informatique",
       logo: require("./assets/images/stanfordLogo.png"),
-      subHeader: "Bachelor of Science in Computer Science",
-      duration: "September 2013 - April 2017",
-      desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
-      descBullets: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit"]
+      subHeader: "Diplôme de Technicien Supérieur",
+      duration: "2005",
+      desc: "Formation technique en informatique."
     }
   ]
 };
 
-// Your top 3 proficient stacks/tech experience
-
+// Tech Stack
 const techStack = {
-  viewSkillBars: true, //Set it to true to show Proficiency Section
+  viewSkillBars: true,
+
   experience: [
     {
-      Stack: "Frontend/Design", //Insert stack or technology you have experience in
-      progressPercentage: "90%" //Insert relative proficiency in percentage
+      Stack: "Frontend / Intégration",
+      progressPercentage: "90%"
     },
     {
-      Stack: "Backend",
+      Stack: "E-commerce / CMS",
+      progressPercentage: "90%"
+    },
+    {
+      Stack: "Backend / API",
       progressPercentage: "70%"
-    },
-    {
-      Stack: "Programming",
-      progressPercentage: "60%"
     }
   ],
-  displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
+
+  displayCodersrank: false
 };
 
-// Work experience section
-
+// Work Experience
 const workExperiences = {
-  display: true, //Set it to true to show workExperiences Section
+  display: true,
+
   experience: [
     {
-      role: "Software Engineer",
-      company: "Facebook",
+      role: "Chef de Pôle Intégration / Intégrateur Web Senior",
+      company: "MADADEV WEB TOOLS",
       companylogo: require("./assets/images/facebookLogo.png"),
-      date: "June 2018 – Present",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      date: "Depuis 2006",
+
+      desc:
+        "Développement et intégration de sites web et e-commerce pour différents projets clients.",
+
       descBullets: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+        "Intégration de maquettes graphiques et développement responsive",
+        "Développement et personnalisation de boutiques Shopify, Magento et WordPress",
+        "Maintenance, migration et optimisation de sites web",
+        "Développement frontend avec JavaScript et React",
+        "Développement backend et API avec PHP, Node.js et Express",
+        "Organisation et suivi du travail d'une équipe d'intégrateurs"
       ]
-    },
-    {
-      role: "Front-End Developer",
-      company: "Quora",
-      companylogo: require("./assets/images/quoraLogo.png"),
-      date: "May 2017 – May 2018",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    },
-    {
-      role: "Software Engineer Intern",
-      company: "Airbnb",
-      companylogo: require("./assets/images/airbnbLogo.png"),
-      date: "Jan 2015 – Sep 2015",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     }
   ]
 };
 
-/* Your Open Source Section to View Your Github Pinned Projects
-To know how to get github key look at readme.md */
-
+// GitHub Section
 const openSource = {
-  showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
-  display: true // Set false to hide this section, defaults to true
+  showGithubProfile: "false",
+  display: false
 };
 
-// Some big projects you have worked on
-
+// Projects Section
 const bigProjects = {
-  title: "Big Projects",
-  subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
+  title: "Mes projets",
+  subtitle:
+    "Quelques domaines et projets sur lesquels j'interviens.",
+
   projects: [
     {
       image: require("./assets/images/saayaHealthLogo.webp"),
-      projectName: "Saayahealth",
-      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      projectName: "Shopify",
+      projectDesc:
+        "Création et personnalisation de boutiques Shopify, développement Liquid, thèmes, produits, métadonnées et intégration e-commerce.",
       footerLink: [
         {
-          name: "Visit Website",
-          url: "http://saayahealth.com/"
+          name: "GitHub",
+          url: "https://github.com/Andrianimehy"
         }
-        //  you can add extra buttons here.
       ]
     },
+
     {
       image: require("./assets/images/nextuLogo.webp"),
-      projectName: "Nextu",
-      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      projectName: "Magento",
+      projectDesc:
+        "Intégration et maintenance de projets Magento, personnalisation de thèmes, modules, configuration et optimisation.",
       footerLink: [
         {
-          name: "Visit Website",
-          url: "http://nextu.se/"
+          name: "GitHub",
+          url: "https://github.com/Andrianimehy"
+        }
+      ]
+    },
+
+    {
+      image: require("./assets/images/saayaHealthLogo.webp"),
+      projectName: "WordPress / WooCommerce",
+      projectDesc:
+        "Création de sites vitrines et boutiques WooCommerce avec personnalisation de thèmes, contenus et fonctionnalités.",
+      footerLink: [
+        {
+          name: "GitHub",
+          url: "https://github.com/Andrianimehy"
+        }
+      ]
+    },
+
+    {
+      image: require("./assets/images/nextuLogo.webp"),
+      projectName: "React + Node.js",
+      projectDesc:
+        "Développement d'applications web avec React, Vite, Node.js, Express, MySQL, API REST et authentification JWT.",
+      footerLink: [
+        {
+          name: "GitHub",
+          url: "https://github.com/Andrianimehy"
+        }
+      ]
+    },
+
+    {
+      image: require("./assets/images/saayaHealthLogo.webp"),
+      projectName: "Performance Web & SEO",
+      projectDesc:
+        "Optimisation des performances, responsive design, chargement des ressources, SEO technique et amélioration des Core Web Vitals.",
+      footerLink: [
+        {
+          name: "GitHub",
+          url: "https://github.com/Andrianimehy"
         }
       ]
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+
+  display: true
 };
 
 // Achievement Section
-// Include certificates, talks etc
-
+// IMPORTANT : AchievementCard.js utilise cardInfo.footer.map()
+// donc chaque carte doit obligatoirement avoir footer: []
 const achievementSection = {
-  title: emoji("Achievements And Certifications 🏆 "),
+  title: emoji("Compétences & réalisations 💻"),
+
   subtitle:
-    "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
+    "Quelques domaines dans lesquels j'interviens en tant qu'intégrateur web senior.",
 
   achievementsCards: [
     {
-      title: "Google Code-In Finalist",
-      subtitle:
-        "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
+      title: "Intégration Web",
+      description:
+        "Intégration de maquettes graphiques, responsive design, animations et interfaces modernes.",
       image: require("./assets/images/codeInLogo.webp"),
-      imageAlt: "Google Code-In Logo",
-      footerLink: [
-        {
-          name: "Certification",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing"
-        },
-        {
-          name: "Award Letter",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing"
-        },
-        {
-          name: "Google Code-in Blog",
-          url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html"
-        }
-      ]
-    },
-    {
-      title: "Google Assistant Action",
-      subtitle:
-        "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
-      image: require("./assets/images/googleAssistantLogo.webp"),
-      imageAlt: "Google Assistant Action Logo",
-      footerLink: [
-        {
-          name: "View Google Assistant Action",
-          url: "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en"
-        }
-      ]
+      imageAlt: "Intégration Web",
+      footer: []
     },
 
     {
-      title: "PWA Web App Developer",
-      subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
+      title: "E-commerce",
+      description:
+        "Création, personnalisation et maintenance de boutiques Shopify, Magento et WooCommerce.",
+      image: require("./assets/images/googleAssistantLogo.webp"),
+      imageAlt: "E-commerce",
+      footer: []
+    },
+
+    {
+      title: "Frontend Development",
+      description:
+        "Développement d'interfaces avec JavaScript, React et les technologies frontend modernes.",
       image: require("./assets/images/pwaLogo.webp"),
-      imageAlt: "PWA Logo",
-      footerLink: [
-        {name: "Certification", url: ""},
-        {
-          name: "Final Project",
-          url: "https://pakistan-olx-1.firebaseapp.com/"
-        }
-      ]
+      imageAlt: "Frontend Development",
+      footer: []
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+
+  display: true
 };
 
 // Blogs Section
-
 const blogSection = {
   title: "Blogs",
+
   subtitle:
-    "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
-  displayMediumBlogs: "true", // Set true to display fetched medium blogs instead of hardcoded ones
-  blogs: [
-    {
-      url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
-      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
-      description:
-        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
-    },
-    {
-      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
-      title: "Why REACT is The Best?",
-      description:
-        "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
-    }
-  ],
-  display: true // Set false to hide this section, defaults to true
+    "Articles et contenus techniques autour du développement web.",
+
+  displayMediumBlogs: "false",
+
+  blogs: [],
+
+  display: false
 };
 
-// Talks Sections
-
+// Talks Section
 const talkSection = {
   title: "TALKS",
+
   subtitle: emoji(
-    "I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅"
+    "Partage d'expérience et de connaissances autour du développement web."
   ),
 
-  talks: [
-    {
-      title: "Build Actions For Google Assistant",
-      subtitle: "Codelab at GDG DevFest Karachi 2019",
-      slides_url: "https://bit.ly/saadpasta-slides",
-      event_url: "https://www.facebook.com/events/2339906106275053/"
-    }
-  ],
-  display: true // Set false to hide this section, defaults to true
+  talks: [],
+
+  display: false
 };
 
 // Podcast Section
-
 const podcastSection = {
   title: emoji("Podcast 🎙️"),
-  subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
 
-  // Please Provide with Your Podcast embeded Link
-  podcast: [
-    "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"
-  ],
-  display: true // Set false to hide this section, defaults to true
+  subtitle: "Contenus autour du développement web et de la technologie.",
+
+  podcast: [],
+
+  display: false
 };
 
 // Resume Section
 const resumeSection = {
-  title: "Resume",
-  subtitle: "Feel free to download my resume",
+  title: "Mon CV",
 
-  // Please Provide with Your Podcast embeded Link
-  display: true // Set false to hide this section, defaults to true
-};
-
-const contactInfo = {
-  title: emoji("Contact Me ☎️"),
   subtitle:
-    "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-0000000000",
-  email_address: "saadpasta70@gmail.com"
+    "Consultez mon parcours professionnel et mes compétences.",
+
+  display: true
 };
 
-// Twitter Section
+// Contact
+const contactInfo = {
+  title: emoji("Contactez-moi ☎️"),
 
+  subtitle:
+    "Vous avez un projet web, e-commerce ou une mission d'intégration ? Contactez-moi.",
+
+  number: "",
+  email_address: ""
+};
+
+// Twitter
 const twitterDetails = {
-  userName: "twitter", //Replace "twitter" with your twitter username without @
-  display: true // Set true to display this section, defaults to false
+  userName: "twitter",
+  display: false
 };
 
-const isHireable = false; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
+// Hiring
+const isHireable = true;
 
 export {
   illustration,
